ac4c4486220bfc559d909e0fd764a179d1f5011f
iq2glupost-cpu <iq2glupost@gmail.com>
2026-03-21 00:46:22 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,21 @@
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "API radi"
-    }
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Postavljamo statusni kod
+        self.send_response(200)
+        # Govorimo pregledaču da vraćamo JSON
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+        # Tvoj odgovor
+        odgovor = {
+            "statusCode": 200,
+            "body": "API radi"
+        }
+        
+        # Šaljemo odgovor
+        self.wfile.write(json.dumps(odgovor).encode('utf-8'))
+        return
+
